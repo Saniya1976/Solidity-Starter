60b20017d6b00be986d3b34adfd55cb9ae826580
Saniya1976 <saniyakumari1976@gmail.com>
2025-09-20 14:07:01 +0530

    twitter contract

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -1,28 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30 ;
-
 contract Twitter{
+    uint16 max_length=280;
     struct Tweet{
         address author;
         string content;
         uint256 likes;
         uint256 timestamp;
-
     }
     mapping (address=>Tweet[]) public tweets;
     function createTweet (string memory _tweet) public {
+        require(byte(_tweet).length==0,"tweet must not be empty")
+        require(byte(_tweet).length<=max_length,"tweet must be less than 280 characters")
         Tweet memory newTweet=Tweet({
-            author:msg.sender;
-            content:_tweet;
-            likes:0;
-            timestamp:block.timestamp;
+            author:msg.sender,
+            content:_tweet,
+            likes:0,
+            timestamp:block.timestamp
         });
         tweets[msg.sender].push(newTweet);
     }
-    function getTweet(address _owner,uint _i) public view returns(string memory ){
-        return tweets[_owner][_i];
+    function getTweet(uint _i) public view returns(Tweet  memory ){
+        return tweets[msg.sender][_i];
     }
-    function getAllTweet(address _owner) public view returns(string[] memory){
-        return tweets[_owner];
+    function getAllTweet() public view returns(Tweet[] memory){
+        return tweets[msg.sender];
     }
 }
