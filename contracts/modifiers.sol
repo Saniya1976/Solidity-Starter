8c06d7a0a15d48ea681f9dc0d53f09728cba7cb4
Saniya1976 <saniyakumari1976@gmail.com>
2025-09-22 22:37:02 +0530

    modifier.sol

--- contracts/modifiers.sol
+++ contracts/modifiers.sol
@@ -1,2 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30 ;
+
+contract test{
+    address public owner;
+
+constructor(){
+    owner=msg.sender;
+}
+modifier onlyowner(){
+    require(msg.sender==owner,"u are not the owner");
+    _;
+}
+function changeOwner(address newOwner) public onlyowner{
+    owner=newOwner;
+}
+}
