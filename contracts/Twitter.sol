56d6320dcbe27ddeb7d3bf6c0cf0f273202408f0
Saniya1976 <saniyakumari1976@gmail.com>
2025-09-23 19:43:21 +0530

    twitter dapp change owner

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -1,24 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30 ;
 contract Twitter{
-    uint16 max_length=280;
+    uint16 public max_length=280;
     struct Tweet{
+        uint256 id;
         address author;
         string content;
         uint256 likes;
         uint256 timestamp;
     }
     mapping (address=>Tweet[]) public tweets;
+    address public owner;
+    constructor(){
+         owner=msg.sender;
+    }
+    modifier onlyOwner(){
+        require(msg.sender==owner,"U ARE NOT THE OWNER");
+        _;
+    }
+    function changetweetlength(uint16 newtweetlength)public onlyOwner{
+        max_length=newtweetlength;
+    }
     function createTweet (string memory _tweet) public {
         require(bytes(_tweet).length==0,"tweet must not be empty");
         require(bytes(_tweet).length<=max_length,"tweet must be less than 280 characters");
         Tweet memory newTweet=Tweet({
+            id:tweets[msg.sender].length,
             author:msg.sender,
             content:_tweet,
             likes:0,
             timestamp:block.timestamp
         });
         tweets[msg.sender].push(newTweet);
+    }
+    function likeTweet(address author,uint256 id) external{
+        require(tweets[author][id].id==id,"TWEET DOESN'T EXISTS");
+        tweets[author][id].likes++;
+    }
+    function unLikeTweet(address author,uint256 id) external{
+        require(tweets[author][id].id==id,"TWEET DOESN'T EXISTS");
+        require(tweets[author][id].likes>0,"TWEET DOESN'T HAVE LIKES");
+        tweets[author][id].likes--;
     }
     function getTweet(uint _i) public view returns(Tweet  memory ){
         return tweets[msg.sender][_i];
