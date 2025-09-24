2767c695d78c83b987e0bd189ec48fbc92a4c477
Saniya1976 <saniyakumari1976@gmail.com>
2025-09-24 23:42:40 +0530

    event exercise

---
+++ contracts/event-exercise.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30 ;
+
+contract eventExample{
+    event newUserRegistered(address indexed user, string username);
+    struct User{
+        uint256 age;
+        string username;
+    }
+    mapping(address=>User) public users;
+    function registerUser(string memory _username , uint256 _age){
+        user storage newUser= users[msg.sender];
+    }
+}
